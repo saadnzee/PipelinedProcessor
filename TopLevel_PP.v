5ad28ad1cbb2840d28245222f78e3d1a852564f6
Saad Naeem <123929383+saadnzee@users.noreply.github.com>
2024-06-14 11:17:42 +0500

    Update TopLevel_PP.v

--- TopLevel_PP.v
+++ TopLevel_PP.v
@@ -1,25 +1,26 @@
 module TopLevel_PP(
  output [31:0] PC_Out,
- output [31:0] Instr,
+ output [31:0] Instr,		// Instruction that is fetched in each cycle
  output reg [31:0] Rs_Val,	// Goes into ALU
  output reg [31:0] Rt_Val,	// Goes into ALU
  input clk,
  input rst,
- input enable_forwarding
+ input enable_forwarding	// if 1 forwarding is done, if 0 we stall
 );
 
-wire [31:0] Rs,Rt;	// Value read at ID Stage
+wire [31:0] Rs,Rt;		// Value read at ID Stage
 reg PCWrite = 1'b1;
 
-wire PCSrc; // Normal PC or Branch Address
-wire [31:0] Jump_Address; // PC for Jump Goes in ID/EX Register
+wire PCSrc; 			// if 0/X Normal PC or if 1 then Branch Address
+wire [31:0] Jump_Address; 	// PC for Jump
 wire Branch;
-reg [31:0] write_data;
+reg [31:0] write_data;		// Either ALUResult or Read Data from RAM
 
 wire jump;
-wire [31:0] PC_EX;	// For Branch (Address)
+wire [31:0] PC_EX;		// For Branch (Address)
+wire [31:0] im;
 
-PC_PP PP (.clk(clk), .rst(rst), .PC_Out(PC_Out), .PCSrc(PCSrc), .PC_EX(PC_EX), .Jump_Address(Jump_Address), .jump(jump), .PCWrite(PCWrite));
+PC_PP PP (.clk(clk), .rst(rst), .PC_Out(PC_Out), .PCSrc(PCSrc), .PC_Branch(PC_EX), .Jump_Address(Jump_Address), .jump(jump), .PCWrite(PCWrite));
 
 ROM_PP RP (.PC_In(PC_Out), .Instr(Instr));
 
@@ -28,6 +29,7 @@ wire [31:0] PC_ID, Instr_ID;
 reg rst_IF_ID = 1'b0;
 reg we_IF_ID = 1'b1;	// FOR HAZARD DETECTION (LW,xx)
 
+// Flush Case when Branch
 always@* begin
 if (PCSrc==1'b1) 
  rst_IF_ID <= 1'b1;
@@ -42,11 +44,9 @@ wire [5:0] opcode;
 wire [25:0] jump_address_temp;
 wire [15:0] im_temp;
 
-Decoder_PP DP (.Instr(Instr_ID), .im_temp(im_temp), .jump_address_temp(jump_address_temp), .opcode(opcode), .rs(rs), .rt(rt), .rd(rd), .shamt(shamt));
+Decoder_PP DP (.Instr(Instr_ID), .im_temp(im_temp), .jump_address_temp(jump_address_temp), .opcode(opcode), .rs(rs), .rt(rt), .rd(rd), .shamt(shamt), .rst(rst));
 
-wire [31:0] im;
 assign im = {{16{im_temp[15]}},im_temp};
-
 assign Jump_Address = {{PC_Out[31:26]},jump_address_temp};
 
 wire [1:0] ALUOp;
@@ -55,20 +55,20 @@ wire RegDst, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite;
 Control_PP CP (.opcode(opcode), .ALUOp(ALUOp), .RegDst(RegDst), .Branch(Branch), .MemRead(MemRead), .MemtoReg(MemtoReg), .MemWrite(MemWrite), .ALUSrc(ALUSrc), 
 .RegWrite(RegWrite), .jump(jump));
 
-reg [4:0] rd_sel;
+reg [4:0] rd_sel;	// Either rd (R-Type) or rt (I-Type)
 wire RegWrite_WB;
 wire [4:0] rd_WB;
 
 RegisterFile_PP RFP (.rs(rs), .rt(rt), .rd(rd_WB), .clk(clk), .rst(rst), .RegWrite(RegWrite_WB), .Data_In(write_data), .Rs(Rs), .Rt(Rt));
-wire [31:0] data_lw;
 
+wire [31:0] data_lw;
 wire [31:0] Rs_EX, Rt_EX, im_EX; 
 wire [4:0] rt_EX, rd_EX, rs_EX;
 
-reg rst_ID_EX;
+reg rst_ID_EX = 1'b0;
 
 wire [31:0] PC_Result;
-assign PC_Result = PC_ID + im;
+assign PC_Result = PC_ID + im;		// In decode stage, PC + Immediate
 
 ID_EX_Register IED (.PC_In(PC_Result), .Rs_In(Rs), .Rt_In(Rt), .im_In(im), .rt_In(rt), .rd_In(rd), .rs_In(rs), .PC_Out(PC_EX), .Rs_Out(Rs_EX), .Rt_Out(Rt_EX), .im_Out(im_EX), .rt_Out(rt_EX),
 .rd_Out(rd_EX), .rs_Out(rs_EX), .clk(clk), .rst(rst), .rst_ID_EX(rst_ID_EX));
@@ -78,7 +78,7 @@ wire [5:0] opcode_EX;
 wire RegDst_EX, MemRead_EX, MemtoReg_EX, MemWrite_EX, ALUSrc_EX, RegWrite_EX, Branch_EX;
 
 ID_EX_Control IEC (.ALUOp_Out(ALUOp_EX), .RegDst_Out(RegDst_EX), .MemRead_Out(MemRead_EX), .MemtoReg_Out(MemtoReg_EX), .MemWrite_Out(MemWrite_EX), .ALUSrc_Out(ALUSrc_EX), .RegWrite_Out(RegWrite_EX),
-.Branch_Out(Branch_EX), .opcode_Out(opcode_EX), .clk(clk), .rst(rst), .ALUOp_In(ALUOp), .RegDst_In(RegDst), .MemRead_In(MemRead), .MemtoReg_In(MemtoReg), .MemWrite_In(MemWrite), .ALUSrc_In(ALUSrc), .RegWrite_In(RegWrite),
+.Branch_Out(Branch_EX), .opcode_Out(opcode_EX), .clk(clk), .rst(rst_ID_EX), .ALUOp_In(ALUOp), .RegDst_In(RegDst), .MemRead_In(MemRead), .MemtoReg_In(MemtoReg), .MemWrite_In(MemWrite), .ALUSrc_In(ALUSrc), .RegWrite_In(RegWrite),
 .Branch_In(Branch), .opcode_In(opcode));
 
 wire zero_flag;
@@ -94,7 +94,7 @@ else begin
 end
 end
 
-reg [31:0] Rt_ALU;
+reg [31:0] Rt_ALU;	// Either Rt (R-Type) or Im (I-Type)
 
 always@* begin
 if (ALUSrc_EX == 1'b0) begin
@@ -116,6 +116,7 @@ wire [31:0] ALUResult_WB;
 
 always@* begin
 
+// LW Type Dependency (One Necessary Stall)
 if(enable_forwarding==1'b1) begin
  if(MemRead_EX == 1'b1) begin
   if ((rt_EX == rs) || (rt_EX == rt)) begin
@@ -136,6 +137,7 @@ if(enable_forwarding==1'b1) begin
  end
 end
 
+// R-Type Dependency Case (Stall must be mantained until Register File is updated and we can read the correct/required data)
 if (enable_forwarding==1'b0) begin
  if(RegWrite_EX == 1'b1) begin
   if ((rd_EX == rs) || (rd_EX == rt)) begin
@@ -144,13 +146,13 @@ if (enable_forwarding==1'b0) begin
    rst_ID_EX <= 1;
   end
  end
-  if ((Rs == ALUResult_WB) || (Rt == ALUResult_WB)) begin
+ if ((Rs == ALUResult_WB) || (Rt == ALUResult_WB)) begin
    we_IF_ID <= 1;
    PCWrite <= 1;
    rst_ID_EX <= 0;
-  end
- //end
+ end
  
+// LW Dependency Case (Stall must be mantained until Register File is updated and we can read the correct/required data)
  if (MemRead_EX == 1'b1) begin
   if ((rt_EX == rs) || (rt_EX == rt)) begin
    we_IF_ID <= 0;
@@ -158,18 +160,16 @@ if (enable_forwarding==1'b0) begin
    rst_ID_EX <= 1;
   end
  end
-  if ((Rs == data_lw) || (Rt == data_lw)) begin
+ if ((Rs == data_lw) || (Rt == data_lw)) begin
    we_IF_ID <= 1;
    PCWrite <= 1;
    rst_ID_EX <= 0;
-  end
- //end
+ end
 
   Rs_Val <= Rs_EX;
   Rt_Val <= Rt_EX;
 
 end
-
 end
 
 
@@ -182,6 +182,7 @@ end
 // ******************************
 // *****For Data Forwarding*****
 // ******************************
+
 wire MemtoReg_WB;
 
 wire [31:0] ALUResult_MEM;
@@ -190,15 +191,15 @@ wire [4:0] rd_MEM;
 // For Rs
 always@* begin
  if (enable_forwarding==1'b1) begin
-  if(rd_MEM == rs_EX)
+  if(rd_MEM == rs_EX)			// Type-1
    Rs_Val <= ALUResult_MEM;
-  else if (rd_WB == rs_EX)
+  else if (rd_WB == rs_EX)		// Type-2
    Rs_Val <= ALUResult_WB;
   else 
    Rs_Val <= Rs_EX;
  
   if (MemtoReg_WB==1'b0) begin
-   if (rd_WB == rs_EX)
+   if (rd_WB == rs_EX)			// In this case rd_WB contains rt/destination for lw
     Rs_Val <= data_lw;
   end
  end
@@ -208,15 +209,15 @@ end
 always@* begin
 if (enable_forwarding==1'b1) begin
  if(rd_MEM == rt_EX)
-  Rt_Val <= ALUResult_MEM;
+  Rt_Val <= ALUResult_MEM;		// Type-1
  else if (rd_WB == rt_EX)
-  Rt_Val <= ALUResult_WB;
+  Rt_Val <= ALUResult_WB;		// Type-2
  else 
   Rt_Val <= Rt_ALU;
 
  if (MemtoReg_WB==1'b0) begin
   if (rd_WB == rt_EX)
-   Rt_Val <= data_lw;
+   Rt_Val <= data_lw;			// In this case rd_WB contains rt/destination for lw		
  end
 end
 end
@@ -225,6 +226,25 @@ end
 // ******************************
 // ******************************
 
+reg rst_EX_MEM, rst_MEM_WB = 1'b0;
+// Flush Case when Branch
+always@* begin
+ if (PCSrc==1'b1) begin
+  rst_ID_EX = 1'b1;
+ end
+ else begin
+  rst_ID_EX = 1'b0;
+ end
+end
+
+always@* begin
+ if (jump==1'b1)
+  rst_IF_ID = 1'b1;
+ else
+  rst_IF_ID = 1'b0;
+end
+
+// Rt_Val should be equal to Rt_ALU in this case Im for all I-Type Instructions
 always@* begin
  if (opcode_EX == 6'd35 || opcode_EX == 6'd43 || opcode_EX == 6'd4 || opcode_EX == 6'd10) begin
   Rt_Val <= Rt_ALU;
@@ -233,35 +253,34 @@ end
 
 ALU_Control_PP ACP (.ALUOp(ALUOp_EX), .funct(funct_in), .ALU_opcode(ALU_opcode));
 
-wire [31:0] ALUResult;
+wire [31:0] ALUResult;		// ALU Result at Execute Stage
 
 ALU_PP AP (.ALUResult(ALUResult), .zero_flag(zero_flag), .ALU_opcode(ALU_opcode), .Rs1(Rs_Val),.Rs2(Rt_Val));
 
 wire MemRead_MEM, MemtoReg_MEM, MemWrite_MEM, RegWrite_MEM, jump_MEM, Branch_MEM;
 
 EX_MEM_Control EMC(.MemRead_Out(MemRead_MEM), .MemtoReg_Out(MemtoReg_MEM), . MemWrite_Out(MemWrite_MEM), . RegWrite_Out(RegWrite_MEM),
-.clk(clk), .rst(rst), .MemRead_In(MemRead_EX), .MemtoReg_In(MemtoReg_EX), .MemWrite_In(MemWrite_EX), .RegWrite_In(RegWrite_EX));
+.clk(clk), .rst(rst_EX_MEM), .MemRead_In(MemRead_EX), .MemtoReg_In(MemtoReg_EX), .MemWrite_In(MemWrite_EX), .RegWrite_In(RegWrite_EX));
 
 wire [31:0] Rt_MEM;
 wire zero_flag_MEM;
 
-
 reg [31:0] Rt_Write;
 
+// LW and SW Dependency
 always@* begin
  if (MemtoReg_WB==1'b0) begin
   if (MemWrite_EX == 1'b1) begin
+    Rt_Write <= Rt_EX;
    if (rd_WB == rt_EX)
     Rt_Write <= data_lw;
   end
-  else
-   Rt_Write <= Rt_EX;
  end
 end
 
 
 EX_MEM_Register EMR (.Rt_Out(Rt_MEM), .rd_Out(rd_MEM), .ALUResult_Out(ALUResult_MEM),
-.clk(clk), .rst(rst), .Rt_In(Rt_Write), .rd_In(rd_sel), .ALUResult_In(ALUResult));
+.clk(clk), .rst(rst_EX_MEM), .Rt_In(Rt_Write), .rd_In(rd_sel), .ALUResult_In(ALUResult));
 
 wire [31:0] read_data;
 
@@ -274,6 +293,7 @@ MEM_WB_Control MWC (.MemtoReg_Out(MemtoReg_WB), .RegWrite_Out(RegWrite_WB), .clk
 MEM_WB_Register MWR (.read_data_Out(data_lw), .rd_Out(rd_WB), .ALUResult_Out(ALUResult_WB), .clk(clk), .rst(rst), .read_data_In(read_data),
 .rd_In(rd_MEM), .ALUResult_In(ALUResult_MEM));
 
+// if MemtoReg = 1, ALUResult goes into Regfile otherwise data from RAM goes.
 always@* begin
  if(MemtoReg_WB == 1'b1)
   write_data <= ALUResult_WB;
@@ -282,4 +302,3 @@ always@* begin
 end
 
 endmodule
-
